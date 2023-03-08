b945db31377e8a9dda72cccccff535b138d4f0d4
Natiman58 <99422296+Natiman58@users.noreply.github.com>
2023-03-08 08:40:18 +0300

    make the response json

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -6,23 +6,23 @@ class UsersController {
     const { email, password } = req.body;
 
     if (!email) {
-      return res.status(400).send({ error: 'Missing email' });
+      return res.status(400).json({ error: 'Missing email' });
     }
 
     if (!password) {
-      return res.status(400).send({ error: 'Missing password' });
+      return res.status(400).json({ error: 'Missing password' });
     }
 
     const userEmail = await dbClient.db.collection('users').findOne({ email });
     if (userEmail) {
-      return res.status(400).send({ error: 'Already exist' });
+      return res.status(400).json({ error: 'Already exist' });
     }
 
     const hashedpwd = sha1(password);
     const result = await dbClient.db.collection('users').insertOne({ email, password: hashedpwd });
 
     const user = { _id: result.insertedId, email };
-    return res.status(201).send(user);
+    return res.status(201).json(user);
   }
 }
 
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 const port = process.env.PORT || 5000;
-const router = require('./routes/index');
+const routes = require('./routes/index');
 
 // to handle url parsing(to extract pwd and email)
 // app.use(bodyParser.urlencoded({ extended: true }));---deprecated
@@ -12,7 +12,7 @@ const router = require('./routes/index');
 app.use(express.json({ limit: '20mb' }));
 app.use(express.urlencoded({ extended: false, limit: '20mb' }));
 
-app.use('/', router);
+app.use('/', routes);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
